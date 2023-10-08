f4a71a8b81c399c407abf21f4effb16dc7c5b93f
Swarnim B <swarnim14.9@hotmail.com>
2023-10-08 21:26:15 +0530

    added a small improvement to bubble sort

--- sorting/bubble-sort.go
+++ sorting/bubble-sort.go
@@ -1,10 +1,15 @@
 package sorting
 
 func bubbleSort(arr []int) {
+	swapped := false
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < len(arr)-i-1; j++ {
 			if arr[j] > arr[j+1] {
+				swapped = true
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+			if swapped == false {
+				break
 			}
 		}
 	}
