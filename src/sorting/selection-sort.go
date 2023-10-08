0f3424b10fe61df3652f6867f517145111d8b1d6
Swarnim B <swarnim14.9@hotmail.com>
2023-10-08 18:10:56 +0530

    selection sort

--- src/sorting/selection-sort.go
+++ src/sorting/selection-sort.go
@@ -4,26 +4,25 @@ import (
 	"fmt"
 )
 
-func selectionSort(input_list []int) []int {
-	sorted_list := input_list
-	for i := 0; i < len(sorted_list); i++ {
+// sorting the list without creating a new one
+func selectionSort(input_list []int) {
+	for i := 0; i < len(input_list); i++ {
 		jMin := i
-		for j := i + 1; j < len(sorted_list); j++ {
-			if sorted_list[j] < sorted_list[jMin] {
+		for j := i + 1; j < len(input_list); j++ {
+			if input_list[j] < input_list[jMin] {
 				jMin = j
 			}
 		}
-		if jMin != 1 {
-			sorted_list[i], sorted_list[jMin] = sorted_list[jMin], sorted_list[i]
+		if jMin != i {
+			input_list[i], input_list[jMin] = input_list[jMin], input_list[i]
 		}
 	}
-	return sorted_list
 }
 
 func main() {
-	unsorted_list := []int{8, 2, 56, 8, 74, 1}
-	sorted_list := selectionSort(unsorted_list)
+	unsorted_list := []int{74, 13, 96, 31, 909}
+	selectionSort(unsorted_list)
 
 	fmt.Println("Sorted List:")
-	fmt.Println(sorted_list)
+	fmt.Println(unsorted_list) //Sorted in place
 }
